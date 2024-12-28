47f8edbff5cc345b6f421d58ad4e04862dabb1bf
Yash Tiwari <yashvtiwari04@gmail.com>
2024-12-27 19:19:14 -0600

    tsv test

--- Source/GrammarGenerator.h
+++ Source/GrammarGenerator.h
@@ -1,11 +1,14 @@
 #ifndef _GRAMMAR_GENERATOR_H
 #define _GRAMMAR_GENERATOR_H
 
-#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 #include <string>
+#include <vector>
 
 /*
-    The grammar generator parses a csv file to understand the languege implementation.
+    The grammar generator parses a csv file to understand the language implementation.
     Impletations will be done using a standard dictionary to describe how functions,
     internal memory structures, and other operations behave
 */
@@ -17,8 +20,10 @@ class GrammarGenerator {
         GrammarGenerator();
         ~GrammarGenerator();
 
-    private:
+        void readFile(std::string langTemplate);
 
+    private: 
+    
         // ------ PARAMETERS ------------
 
 };
---
+++ Test/testGG.cpp
@@ -0,0 +1,9 @@
+#include "Source/GrammarGenerator.h"
+
+int main() {
+    GrammarGenerator gg;
+
+    gg.readFile("/Definition/test.tsv");
+
+    return 0;
+}
