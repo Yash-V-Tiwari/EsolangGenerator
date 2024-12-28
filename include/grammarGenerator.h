3230eccc54c2723ea30f474f53bfd82f57ce6976
Yash Tiwari <yashvtiwari04@gmail.com>
2024-12-27 18:36:46 -0600

    minor changes

---
+++ include/grammarGenerator.h
@@ -0,0 +1,26 @@
+#ifndef _GRAMMAR_GENERATOR_H
+#define _GRAMMAR_GENERATOR_H
+
+#include <vector>
+#include <string>
+
+/*
+    The grammar generator parses a csv file to understand the languege implementation.
+    Impletations will be done using a standard dictionary to describe how functions,
+    internal memory structures, and other operations behave
+*/
+
+class GrammarGenerator {
+
+    public:
+
+        GrammarGenerator();
+        ~GrammarGenerator();
+
+    private:
+
+        // ------ PARAMETERS ------------
+
+};
+
+#endif /* _GRAMMAR_GENERATOR_H */
